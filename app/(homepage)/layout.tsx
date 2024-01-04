0815263293f8533551e11eea9279b511b83de2f8
Yingzhou Jiang <34469646+CocoaPebble@users.noreply.github.com>
2024-01-04 16:21:03 -0500

    Remove unnecessary code and delete unused component

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/ui/nav";
 const HomepageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full">
-      
       <Navbar />
       <main className="w-full pt-28 container">{children}</main>
     </div>
